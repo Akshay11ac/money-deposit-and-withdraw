bc4f69425afea641db407ec04093729e98859039
Akshay11ac <aks11ac11@gmail.com>
2021-10-02 15:11:14 +0530

    Header btn css changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,30 @@
 import { BUTTON_TYPE } from "../utility/constant";
 import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
   const buttons = [BUTTON_TYPE.DEPOSIT, BUTTON_TYPE.WITHDRAW];
   const route = useHistory();
+  const [activePage, setActivePage] = useState();
 
   const handleBtnClick = (routeTo) => {
     let url = routeTo === BUTTON_TYPE.DEPOSIT ? "/" : routeTo;
     route.push(url);
+    setActivePage(routeTo);
   };
 
   return (
-    <div>
+    <header className="header-container">
       {buttons.map((data, idx) => (
-        <button key={idx} onClick={() => handleBtnClick(data)}>
+        <button
+          key={idx}
+          onClick={() => handleBtnClick(data)}
+          className={`header-btn ${activePage === data && "active-btn"}`}
+        >
           {data}
         </button>
       ))}
-    </div>
+    </header>
   );
 };
 
