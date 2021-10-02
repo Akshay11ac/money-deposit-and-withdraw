b29198054b1e4af375b0a3b20e23a9670ffcedd4
Akshay11ac <aks11ac11@gmail.com>
2021-10-02 14:42:56 +0530

    Router integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Deposit from "./components/Deposit";
+import WithDraw from "./components/WithDraw";
 
 function App() {
-  return <div className="App">Money Deposit and WithDraw</div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/">
+          {Deposit}
+        </Route>
+        <Route exact path="/WithDraw">
+          {WithDraw}
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
